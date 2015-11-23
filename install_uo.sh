55ac976b3a372742fed77eec70400a1c19043899
0x1p2 <ry4.ball@gmail.com>
2015-11-22 19:09:47 -0600

    Added more comments for viewers to understand.

--- install_uo.sh
+++ install_uo.sh
@@ -20,48 +20,47 @@
 
 clear
 
-UOC_CLIENT_EXE="UOClassicSetup.exe"
-UOS_CLIENT_RAR="UOS_Latest.rar"
-UOS_CLIENT_EXE="UOS_Latest.exe"
+UOC_CLIENT_EXE="UOClassicSetup.exe"	# Assigns the name of the client
+UOS_CLIENT_RAR="UOS_Latest.rar"		# Name of the .rar file for UO Steam
+UOS_CLIENT_EXE="UOS_Latest.exe"		# Name of the UO Steam application (what is extracted from .rar)
 
-UOC_CLIENT_URL="http://web.cdn.eamythic.com/us/uo/installers/20120309/UOClassicSetup_7_0_24_0.exe"
-UOS_CLIENT_URL="http://www.uoforever.com/files/UOS_Latest.rar"
+UOC_CLIENT_URL="http://web.cdn.eamythic.com/us/uo/installers/20120309/UOClassicSetup_7_0_24_0.exe"	# URL to download from
+UOS_CLIENT_URL="http://www.uoforever.com/files/UOS_Latest.rar"						# URL to download from
 INSTALL_DIR="${PWD}/ultima_install"
-full_install="false"
 
-INS="[\x1B[1;36m**\x1B[0m]"
-SUC="[\x1B[36m+\x1B[0m]"
-BAD="[\x1B[31m-\x1B[0m]"
+INS="[\x1B[1;36m**\x1B[0m]"	# Fancy prefix for a string display of a install: [**]
+SUC="[\x1B[36m+\x1B[0m]"	# Fancy prefix for a string display of successful return: [+]
+BAD="[\x1B[31m-\x1B[0m]"	# Fancy prefix for a string display of a failture return: [-]
 
 
 function main
 {
-	check_progs
+	check_progs					# Checks the required programs and makes sures that are installed... if not
+							#   it will exit the application and notify what is needed.
+	create_install_dir				# Creates the installation directory (This directory houses the download files.)
+	if [[ ${execution_success} == "true" ]]; then	# If the directory was succesfully created...
+		create_install_dir			# Navigates into the directory. This is a lame redundancy but is more handy
+	fi						#   in the following functions. Just keeping it of a similar design.
 
-	create_install_dir
-	if [[ ${execution_success} == "true" ]]; then
-		create_install_dir
+	UOC_install					# Downloads and installs the Ultima Online Classic Client.
+	if [[ ${execution_success} == "true" ]]; then	# If the download was successful (Double checks and makes sure file exist):
+		UOC_install				# Launches the wizard for installing UO. Accept the defaults.
+	fi						#   It will then launch UO to start patching. The script will exit here.
+
+	UOS_install					# After a relaunch of the script... Downloads the UO Steam client.
+	if [[ ${execution_success} == "true" ]]; then	# If the download was successfull (verifies existance):
+		UOS_install				# Launches the wize for installing the client.
 	fi
 
-	UOC_install
-	if [[ ${execution_success} == "true" ]]; then
-		UOC_install
-	fi
+	WINE_fix					# Launches the winecfg menu to fix the window management of the Ultima Client.
 
-	UOS_install
-	if [[ ${execution_success} == "true" ]]; then
-		UOS_install
-	fi
-
-	WINE_fix
-
-	LINK_create
+	LINK_create					# Final options for creating aliases and shortcuts to launching UOS and UO.
 }
 
 
 function check_progs
 {
-	req_progs="wine wget rar"
+	req_progs="wine wget rar"	# Programs that will be iterated through to verify they exist on the system.
 
 	for program in ${req_progs}; do
 		if [[ ! -f /usr/bin/${program} || ! -f /bin/${program} ]]; then	# Checks to verify you have the ability to download the executables (wget)
@@ -70,7 +69,7 @@ function check_progs
 			exit		# This executable is required... can not proceeded with downloading without it
 		else
 			echo -e " ${SUC} ${program} found."
-		fi			#   unless it is done manually....
+		fi
 	done
 }
 
@@ -78,16 +77,14 @@ function check_progs
 function create_install_dir
 {
 	execution_success="false"
-	if [[ ! -d ultima_install/ ]]; then
+	if [[ ! -d ultima_install/ ]]; then	
 		echo -e " ${BAD} Directory: ultima_install/ does not exist... Creating."
-		mkdir ultima_install			# Creates the directory.
+		mkdir ultima_install		# Creates the directory.
 		echo -e " ${SUC} Directory created."
-		full_install="true"			# Sets the flag "full_install" because install files do not exist.
-
 
 		echo -e " ${BAD} Verifying directory creation was successful..."
 		sleep 1
-		execution_success="true"
+		execution_success="true"	# Sets the flag that will allow for rechecking directory existance and then changing.
 
 	else
 		echo -e "\n ${SUC} Directory: ultima_install/ exists... changing current directory."
@@ -102,16 +99,16 @@ function UOC_install
 	execution_success="false"
 	if [[ ! -f ${UOC_CLIENT_EXE} ]]; then
 		echo -e "\n  ${BAD} File ${UOC_CLIENT_EXE} not found. Downloading."
-		wget -O ${UOC_CLIENT_EXE} ${UOC_CLIENT_URL} > /dev/null 2>&1
+		wget -O ${UOC_CLIENT_EXE} ${UOC_CLIENT_URL} > /dev/null 2>&1		# Downloads the UO Classic Client.
 		sleep 1
 		if [[ -f ${UOC_CLIENT_EXE} ]]; then
 			echo -e " ${SUC} ${UOC_CLIENT_EXE} download complete. "
-			execution_success="true"
+			execution_success="true"					# Set flag for it to install (download success)
 		fi
 	else
 		if [[ ! -d "${HOME}/.wine32/drive_c/Program Files/Electronic Arts/Ultima Online Classic/" ]]; then
 			echo -e "\n ${INS} Launching installation of: ${UOC_CLIENT_EXE}."
-			WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine ${UOC_CLIENT_EXE} > /dev/null 2>&1
+			WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine ${UOC_CLIENT_EXE} > /dev/null 2>&1	# Launch the UO Install.
 			echo -e " ${SUC} Installation complete."
 		fi
 
@@ -119,13 +116,13 @@ function UOC_install
 			echo -e "\n ${INS} Patching the ${UOC_CLIENT_EXE} client."
 			cd "${HOME}/.wine32/drive_c/Program Files/Electronic Arts/Ultima Online Classic/"
 			echo "  Current Directory: ${PWD}"
-			WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine UO.exe > /dev/null 2>&1
+			WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine UO.exe > /dev/null 2>&1	# Launch the Patcher
 			echo -e "\n\n The patcher may take several seconds to launch. After the patch update is"
 			echo -e "complete. Please restart the script to continue the install. It will no duplicate"
 			echo -e "any of the previous installation. Thank you- (Approximately 30seconds)"
 			for ((i = 30; i > -1; i--))
 			do
-				echo -en "\rTime: ${i}  "
+				echo -en "\rTime: ${i}  "	# Count down of 30 seconds... so users don't think it is broken.
 				sleep 1
 			done
 			
@@ -143,25 +140,25 @@ function UOS_install
 	if [[ ! -f ${UOS_CLIENT_RAR} || ! -f ${UOS_CLIENT_EXE} ]]; then
 		if [[ ! -f ${UOS_CLIENT_RAR} ]]; then
 			echo -e "\n  ${BAD} File: ${UOS_CLIENT_RAR} not found... Downloading."
-			wget -O ${UOS_CLIENT_RAR} ${UOS_CLIENT_URL} > /dev/null 2>&1
+			wget -O ${UOS_CLIENT_RAR} ${UOS_CLIENT_URL} > /dev/null 2>&1		# Downloads the UO Steam client.
 			echo -e " ${SUC} Download complete of: ${UOS_CLIENT_RAR}"
 			sleep 1
 		fi
 
 		if [[ -f ${UOS_CLIENT_RAR} && ! -f ${UOS_CLIENT_EXE} ]]; then 
 			echo -e " ${BAD} Unpacking ${UOS_CLIENT_RAR}."
-			rar x ${UOS_CLIENT_RAR} > /dev/null 2>&1
+			rar x ${UOS_CLIENT_RAR} > /dev/null 2>&1				# Extracts the UOS_Latest.exe
 			sleep 1
 			if [[ -f ${UOS_CLIENT_EXE} ]]; then
 				echo -e " ${SUC} Unpackaging of ${UOS_CLIENT_RAR} => ${UOS_CLIENT_EXE} complete."
-				execution_success="true"
+				execution_success="true"	# Flag for installation.
 			fi
 		fi
 	else
 		# # # #  INSTALL
 		if [[ ! -d "${HOME}/.wine32/drive_c/Program Files/UOS" ]]; then
 			echo -e "\n ${INS} Installing ${UOS_CLIENT_EXE}."
-			WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine ${UOS_CLIENT_EXE} > /dev/null 2>&1
+			WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine ${UOS_CLIENT_EXE} > /dev/null 2>&1	# Launches UOS installation process.
 		fi
 	fi
 }
@@ -177,7 +174,7 @@ function WINE_fix
 	echo -e "\n Options to DESELECT on the \"Graphics\" tab: "
 	echo -e "\tAllow the window manager to decorate the windows."
 	echo -e "\tAllow the window manager to control the windows."
-	WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 winecfg > /dev/null 2>&1
+	WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 winecfg > /dev/null 2>&1	# Launches the winecfg to disable window management.
 }
 
 
@@ -189,7 +186,7 @@ function LINK_create
 	echo -n "  Option [y/N]: "
 	read input
 	if [[ ${input} == [yY] || ${input} == [yY][Ee][Ss] ]]; then
-		check_alias
+		check_alias	# If accepted... Check existance, if not- create alias in users default shell.
 	fi
 
 	echo -e "\n Do you want to make a shortcut to the game launch in ${HOME}?"
@@ -198,7 +195,7 @@ function LINK_create
 	echo -n "  Option [y/N]: "
 	read input
 	if [[ ${input} == [yY] || ${input} == [yY][Ee][Ss] ]]; then
-		check_shortcut	
+		check_shortcut	# If accepted... Check existance, if not- create the short for launching UO.
 	fi
 
 	echo -e "\n\n Installation complete! If you want a chance to renable some of these options above, "
@@ -211,12 +208,12 @@ function LINK_create
 
 function check_alias
 {
-	shell=`echo ${SHELL} | awk -F"/" '{print $4}'`
-	if_enabled=`cat ${HOME}/.${shell}rc | grep "alias wine"`
+	shell=`echo ${SHELL} | awk -F"/" '{print $4}'`			# Parses /usr/bin/SHELL for shell name
+	if_enabled=`cat ${HOME}/.${shell}rc | grep "alias wine"`	# Checks for alias inside shells configuration file.
 	if [[ ${if_enabled} != "" ]]; then
-		echo -e "\n ${BAD} Alias exists already.."
+		echo -e "\n ${BAD} Alias exists already.."		# Already exists. MEH.
 	else
-		echo "alias wine='WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine'" >> ${HOME}/.${shell}rc
+		echo "alias wine='WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine'" >> ${HOME}/.${shell}rc	# Appends alias to config file
 		echo -e "\n\n ${SUC} Alias added. \n"
 	fi
 }
@@ -224,13 +221,13 @@ function check_alias
 
 function check_shortcut
 {
-	UO_PATH="${HOME}/ultima.sh"
-	if [[ ! -f ${UO_PATH} ]]; then
-		echo "#!/bin/bash" > ${UO_PATH}
-		echo "cd \"${HOME}/.wine32/drive_c/Program Files/UOS\"" >> ${UO_PATH} 
-		echo "WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine UOS.exe" >> ${UO_PATH}
+	UO_PATH="${HOME}/ultima.sh"								# FILE: ./ultima.sh
+	if [[ ! -f ${UO_PATH} ]]; then	
+		echo "#!/bin/bash" > ${UO_PATH}							# #!/bin/bash
+		echo "cd \"${HOME}/.wine32/drive_c/Program Files/UOS\"" >> ${UO_PATH} 		# cd "${HOME}/.wine32/drive_c/Program Files/UOS"
+		echo "WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine UOS.exe" >> ${UO_PATH}	# WINEPREFIX=${HOME}/.wine32 WINEARCH=win32 wine UOS.exe
 		echo -e "\n${UO_PATH} has been created!"
-		chmod +x ${UO_PATH}
+		chmod +x ${UO_PATH}								# Marks the new file executable.
 		echo "EXECUTE AND LAUNCH UO BY: \"./ultima.sh\" in your home directory."
 	else
 		echo -e " ${BAD} ${UO_PATH} already exists!"
