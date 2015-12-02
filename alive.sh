817aedac45fb524f54ffe05e48b467f7cc6c1e4a
0x1p2 <ry4.ball@gmail.com>
2015-12-02 16:58:51 -0600

    Updated Ping script.

--- alive.sh
+++ alive.sh
@@ -2,6 +2,7 @@
 #syntax: alive.sh <#_of_pings>
 DEADCOUNT=0
 LIVECOUNT=0
+HOST="192.168.1.250"
 x=0
 count="${1}"
 if [[ ${1} == "" ]]; then count=0; fi
@@ -9,10 +10,11 @@ while [[ ${x} -le ${count} ]];
 do
 	#CHECKING="$CHECKING${i} "
 	x=$((x+1))
-	ping -c 1 -t 1 vps > /dev/null 2>&1;
+	ping -c 1 -t 1 ${HOST} > /dev/null 2>&1;
 	if [[ $? -eq 0 ]]; then
 		LIVECOUNT=$((LIVECOUNT+1))
 		echo "${LIVECOUNT}: Alive"
+		sleep 1
 	elif [[ $? -gt 0 ]]; then
 		DEADCOUNT=$((DEADCOUNT+1))
 		echo "${DEADCOUNT}: Dead"
